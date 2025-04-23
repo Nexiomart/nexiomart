9ab8a37005a48c68e4a59c500dc588e425cc8adc
Riyansh Prakash <riyanshprakash007@gmail.com>
2025-04-23 16:52:04 +0530

    resolving issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/nexio-logo.jpg' ;
+import logo from '../assets/Nexio-logo.jpg' ;
 
 const Navbar = () => {
   return (
